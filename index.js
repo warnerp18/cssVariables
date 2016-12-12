ab615b1c00cb732033dd0e1843fb02ef375bf21f
Phil <warnerp18@gmail.com>
2016-12-11 17:27:10 -0800

    add video

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const inputs = document.querySelectorAll('.controls input');
+//const inputs = document.querySelectorAll('.controls input');
 
+var inputs = document.querySelectorAll('.controls input');
 function handleUpdate(){
   const suffix = this.dataset.sizing || '';
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
